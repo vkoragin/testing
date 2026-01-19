1da18b55a5749e8a92c82e3dfc04db16dde91f4e
Victor Koriagin <superiaschik@rambler.ru>
2026-01-20 00:02:58 +0300

    02_reasons_for_testing_exersize

---
+++ src/tests/collection.test.js
@@ -0,0 +1,20 @@
+// @ts-check
+import getFunction from "../functions.js";
+
+const get = getFunction();
+
+// BEGIN (write your solution here)
+if (get({ hello: "world" }, "hello") !== "world") {
+  throw new Error("Функция работает неверно!");
+}
+
+if (get({}, "hello", "kitty") !== "kitty") {
+  throw new Error("Функция работает неверно!");
+}
+
+if (get({ hello: "world" }, "hello", "kitty") !== "world") {
+  throw new Error("Функция работает неверно!");
+}
+
+console.log("Все тесты пройдены!");
+// END
