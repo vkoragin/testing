b06907cd89ad37c28d5cb64349c40143a9b30422
Victor Koriagin <superiaschik@rambler.ru>
2026-01-31 17:56:36 +0300

    03_asserts

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,5 +1,5 @@
 import { strict as assert } from "node:assert";
 import { capitalize } from "../capitalize.js";
 
-assert(capitalize("") === "");
-assert(capitalize("hello") === "Hello");
+assert.equal(capitalize(""), "");
+assert.equal(capitalize("hello"), "Hello");
--- src/tests/collection.test.js
+++ src/tests/collection.test.js
@@ -3,6 +3,6 @@ import getFunction from "../functions.js";
 
 const get = getFunction();
 
-assert(get({ hello: "world" }, "hello") === "world");
-assert(get({}, "hello", "kitty") === "kitty");
-assert(get({ hello: "world" }, "hello", "kitty") === "world");
+assert.equal(get({ hello: "world" }, "hello"), "world");
+assert.equal(get({}, "hello", "kitty"), "kitty");
+assert.equal(get({ hello: "world" }, "hello", "kitty"), "world");
